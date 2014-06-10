fbb60cb243e284be76a24149b61f20a23f1fe8fa
msczepan <msczepan@Zalando-BM0015>
2014-06-10 14:26:37 +0200

    client

---
+++ chat-client/src/chat/client/ChatClient.java
@@ -0,0 +1,23 @@
+/**
+ * @author: matthias.sczepan@zalando-lounge.de
+ */
+package chat.client;
+
+import chat.client.gui.Gui;
+
+/**
+ *
+ * @author msczepan
+ */
+public class ChatClient {
+
+    
+    public static void main(String[] args) {
+        init();
+    }
+    
+    private static void init()
+    {
+        Gui mainWindow = new Gui();
+    }
+}
---
+++ chat-client/src/chat/client/gui/Gui.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat.client.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author msczepan
+ */
+public class Gui extends JFrame{
+    
+    public Gui()
+    {
+        this.setSize(300, 400);
+        this.show();
+        this.setWindowListener();
+    }
+    
+    private void setWindowListener()
+    {
+        this.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+    }
+}
